ee1d5557128d6bbc84cd989f44d82ff6399924c0
Matteo <spartacus990@gmail.com>
2022-10-30 17:34:31 +0100

    add plugins and their config

--- .config/nvim/lua/matteospanio/plugins.lua
+++ .config/nvim/lua/matteospanio/plugins.lua
@@ -1,11 +1,12 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+        install_path })
     vim.cmd [[packadd packer.nvim]]
 end
 
-return require('packer').startup(function ()
+return require('packer').startup(function()
     -- packer manages itself
     use 'wbthomason/packer.nvim'
 
@@ -51,9 +52,14 @@ return require('packer').startup(function ()
 
     use 'norcalli/nvim-colorizer.lua'
 
+    -- ts-autotag requires treesitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }
     -- Autoenclosing tags
     use 'windwp/nvim-autopairs'
-    --use 'windwp/nvim-ts-autotag'
+    use 'windwp/nvim-ts-autotag'
     use 'akinsho/nvim-bufferline.lua'
 
     if packer_bootstrap then
