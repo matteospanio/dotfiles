3a3788eab5bbaed11cafe4590d4d17e8e4929341
Matteo <spartacus990@gmail.com>
2022-10-31 15:58:32 +0100

    lint nvim plugin files

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -204,6 +204,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/matteo/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["tokyonight.nvim"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    url = "https://github.com/folke/tokyonight.nvim"
   }
 }
 
