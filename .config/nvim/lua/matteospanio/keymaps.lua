96f2d3c09bbfb718d1dec289e109dfe20f8c8194
Matteo <spartacus990@gmail.com>
2022-11-05 22:04:15 +0100

    add motion keymaps

--- .config/nvim/lua/matteospanio/keymaps.lua
+++ .config/nvim/lua/matteospanio/keymaps.lua
@@ -1,6 +1,28 @@
 local map = vim.keymap
 
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--  normal_mode = "n"
+--  insert_mode = "i"
+--  visual_mode = "v"
+--  visual_block_mode = "x"
+--  term_mode = "t"
+--  command_mode = "c"
+
+-- Normal
+-- Window navigation
+map.set('n', '<C-h>', '<C-w>h')
+map.set('n', '<C-j>', '<C-w>j')
+map.set('n', '<C-k>', '<C-w>k')
+map.set('n', '<C-l>', '<C-w>l')
+
+-- Resize with arrow
+map.set('n', '<C-Up>', ':resize -2<CR>')
+map.set('n', '<C-Down>', ':resize +2<CR>')
+map.set('n', '<C-Left>', ':vertical resize -2<CR>')
+map.set('n', '<C-Up>', ':vertical resize +2<CR>')
 
 -- Increment/decrement
 map.set('n', '+', '<C-a>')
